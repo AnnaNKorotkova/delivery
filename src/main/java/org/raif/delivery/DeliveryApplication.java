01eebf8f6acf7571c0676f3eb1c0e5397d0cba84
ruaabcn <anna.korotkova@raiffeisen.ru>
2025-12-19 21:05:54 +0300

    Outbox

--- src/main/java/org/raif/delivery/DeliveryApplication.java
+++ src/main/java/org/raif/delivery/DeliveryApplication.java
@@ -6,14 +6,17 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EntityScan(basePackages = {
-        "org.raif.delivery.core.domain.model"
+        "org.raif.delivery.core.domain.model",
+        "org.raif.delivery.adapters.out.postgres.outbox"
 })
 @ConfigurationPropertiesScan
+@EnableScheduling
 public class DeliveryApplication {
 
     public static void main(String[] args) {
